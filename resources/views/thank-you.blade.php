2d5ba80936bfd87157ce8a1773c71870b5557883
Ioan Petrica Canalos <giovanni.canalos@gmail.com>
2026-01-22 17:19:16 +0100

    Ho fatto una piccola modifica sul frontend

--- resources/views/thank-you.blade.php
+++ resources/views/thank-you.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <header class="container-fluid">
         <div class="row d-flex justify-content-center align-items-center header-custom">
-            <div class="col-12 py-5 box-bg text-center text-white">
+            <div class="col-12 py-5 text-center text-white">
                 <h1 class="text-center display-4 title">Grazie per averci contattato</h1>
                 <h2>Abbiamo ricevuto la tua mail</h2>
                 <h3>Riceverai al più presto una risposta</h3>
